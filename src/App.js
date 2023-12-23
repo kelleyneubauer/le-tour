347c96aac2bd908b4840ecd267e11eab27a120e2
Kelley Neubauer <kelley@Kelleys-MBP.lan>
2023-12-23 14:22:26 -0800

    add link to home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import './App.css';
+import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          LE TOUR
-        </p>
-      </header>
+      <p>LE TOUR</p>
+      <p>
+        <div>
+          <Link to="/masher">Button Masher</Link>
+        </div>
+        <div>
+          <Link to="/puzzle">Tipsy Tourney</Link>
+        </div>
+        <div>
+          <Link to="/trivia">Trivia</Link>
+        </div>
+        <div>
+          <Link to="/math">Shy Guy Says</Link>
+        </div>
+        <div>
+          <Link to="/balloons">Balloon Shot</Link>
+        </div>
+      </p>
     </div>
   );
 }
