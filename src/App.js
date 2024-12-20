595117c3cc6f97930ba0a419b2bb44bcad0f38e6
Kelley Neubauer <kelleyneubauer@gmail.com>
2024-12-20 15:26:58 -0800

    update

--- src/App.js
+++ src/App.js
@@ -2,28 +2,31 @@ import "./App.css";
 import { Link } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <p>LE TOUR</p>
-      <p>
-        <div>
-          <Link to="/masher">Button Masher</Link>
+    return (
+        <div className="App">
+            <p>LE TOUR 2024</p>
+            <p>
+                <div>
+                    <Link to="/masher">Button Masher</Link>
+                </div>
+                <div>
+                    <Link to="/puzzle">Tipsy Tourney</Link>
+                </div>
+                <div>
+                    <Link to="/trivia">Trivia</Link>
+                </div>
+                <div>
+                    <Link to="/math">Shy Guy Says</Link>
+                </div>
+                <div>
+                    <Link to="/balloons">Balloon Shot</Link>
+                </div>
+                <div>
+                    <Link to="/wordle">Wordle</Link>
+                </div>
+            </p>
         </div>
-        <div>
-          <Link to="/puzzle">Tipsy Tourney</Link>
-        </div>
-        <div>
-          <Link to="/trivia">Trivia</Link>
-        </div>
-        <div>
-          <Link to="/math">Shy Guy Says</Link>
-        </div>
-        <div>
-          <Link to="/balloons">Balloon Shot</Link>
-        </div>
-      </p>
-    </div>
-  );
+    );
 }
 
 export default App;
